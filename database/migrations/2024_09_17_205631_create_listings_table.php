c2264ba5586b47e46d9a30fb7115632342f6ca66
Han-Me0 <haninjamalmzher@gmail.com>
2024-09-18 13:22:11 +0200

    Switched db configuration from SQLite to Mysql
    Define relationships and attributes for each model
    
    Set up corresponding database migrations

---
+++ database/migrations/2024_09_17_205631_create_listings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('listings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->string('slug');
+            $table->string('company');
+            $table->string('location');
+            $table->string('logo')->nullable();
+            $table->boolean('is_highlited')->default(false);
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+            $table->string("content");
+            $table->string("apply_link");
+
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('listings');
+    }
+};
